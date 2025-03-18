7817ffbd60c67d2b1e0c6a97a33a233188ba6f1e
Aggeliki <aggmegalou@gmail.com>
2025-03-18 19:57:34 +0200

    I created entity Book

---
+++ spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -0,0 +1,39 @@
+package com.luv2code.springbootlibrary.entity;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Book")
+@Data
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name="description")
+    private String dscription;
+
+    @Column(name="copies")
+    private int copies;
+
+    @Column(name="copies_available")
+    private int copiesAvailable;
+
+    @Column(name ="category")
+    private String category;
+
+    @Column(name="img")
+    private String img;
+
+
+}
--- spring-boot-library/src/test/java/com/luv2code/spring_boot_library/SpringBootLibraryApplicationTests.java
+++ spring-boot-library/src/test/java/com/luv2code/spring_boot_library/SpringBootLibraryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.luv2code.spring_boot_library;
+/*package com.luv2code.spring_boot_library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class SpringBootLibraryApplicationTests {
 	}
 
 }
+*/
